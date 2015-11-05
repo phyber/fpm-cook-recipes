6591f6b73d9c58043abd8d3dc98b38da9f93a086
David O'Rourke <david.orourke@gmail.com>
2015-11-05 00:54:22 +0000

    Tabs to spaces for dcadec

--- dcadec/recipe.rb
+++ dcadec/recipe.rb
@@ -1,22 +1,22 @@
 class DCADEC < FPM::Cookery::Recipe
-	description	'DTS Coherent Acoustics decoder with support for HD extensions'
+    description 'DTS Coherent Acoustics decoder with support for HD extensions'
 
-	name		'libdcadec'
-	version		'0.0.1'
-	revision	'1'
+    name        'libdcadec'
+    version     '0.0.1'
+    revision    '1'
 
-	homepage	'https://github.com/foo86/dcadec'
-	source		'https://github.com/foo86/dcadec.git',
-				:with	=> 'git',
-				:branch	=> 'master'
+    homepage    'https://github.com/foo86/dcadec'
+    source      'https://github.com/foo86/dcadec.git',
+        :with   => 'git',
+        :branch => 'master'
 
-	def build
-		make
-	end
+    def build
+        make
+    end
 
-	def install
-		make :install,
-			'DESTDIR' => destdir,
-			'PREFIX'  => '/usr'
-	end
+    def install
+        make :install,
+            'DESTDIR' => destdir,
+            'PREFIX'  => '/usr'
+    end
 end
