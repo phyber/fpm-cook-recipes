22171857498564c7a8ffc27f7e92da41a7959269
David O'Rourke <david.orourke@gmail.com>
2015-11-19 01:14:49 +0000

    Add build_depends for Weechat.

--- weechat/recipe.rb
+++ weechat/recipe.rb
@@ -9,6 +9,24 @@ class WeeChat < FPM::Cookery::Recipe
     source      "https://weechat.org/files/src/weechat-#{version}.tar.bz2"
     sha1        '3d6cac3a92b194819197ee7ed435a0c2e62a7c66'
 
+    build_depends %w(
+        asciidoc
+        ca-certificates
+        cmake
+        gettext
+        libaspell-dev
+        libcurl-ssl-dev
+        libgcrypt20-dev
+        libgnutls28-dev
+        liblua5.2-dev
+        libncursesw5-dev
+        libperl-dev
+        pkg-config
+        python2.7-dev
+        ruby-dev
+        zlib1g-dev
+    )
+
     def before_build
         mkdir_p 'build'
         Dir.chdir 'build'
